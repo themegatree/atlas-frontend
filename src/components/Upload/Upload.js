b96cc35885322efec3275890eaf84e3cdc58598e
unknown <benclayton1@hotmail.com>
2021-08-18 11:57:42 +0100

    enctype added

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -4,12 +4,12 @@ class Upload extends React.Component {
     render() {
         return (
             <div>
-                <form action="/api/fileupload" method="POST">
-                    <input id="choose-file" type="file" accept=".csv" name="file"/>
+                <form action="http://localhost:5000/api/fileupload" method="POST"encType="multipart/form-data" >
+                    <input id="choose-file" type="file" accept=".csv" name="upload"/>
                     <label for="assessment-type">Assessment Type</label>
-                    <select name="assessment-type" id="assessment-type">
-                        <option value="self-assessment">Self Assessment</option>
-                        <option value="module-challenge">Module Challenge</option>
+                    <select name="assessmentType" id="assessment-type">
+                        <option value="selfAssessment">Self Assessment</option>
+                        <option value="moduleChallenge">Module Challenge</option>
                     </select>
                     <button id="upload" type="submit">Upload</button>
                 </form>
