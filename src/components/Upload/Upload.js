7b78d9254d058560b1b0c1a960ad51a0a05a6127
unknown <benclayton1@hotmail.com>
2021-08-19 12:23:16 +0100

    pushing frontend with errors displaying

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, {Component} from "react";
 
-class Upload extends React.Component {
+class Upload extends Component {
+   
+    constructor(props){
+        super(props)
+        this.state = {
+            selectedFile: null,
+            assessmentType: 'selfAssessment',
+            errors: []
+        }
+    }
+
+
+    onFileChange = event => {
+        this.setState({ selectedFile : event.target.files[0] });     
+      };
+
+    setAssessmentType = event => {
+        this.setState({ assessmentType : event.target.value });
+    }
+
+    onFileUpload = () => {
+        const formData = new FormData();
+        console.log(this.state.assessmentType)
+        formData.append(
+          "myFile",
+          this.state.selectedFile,
+          this.state.selectedFile.name,
+        );
+        formData.append(
+            "assessmentType",
+            this.state.assessmentType
+        )
+
+        fetch("http://localhost:5000/api/fileupload",{
+            method: "POST", 
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => this.setState({ errors: data}));
+    }
+
     render() {
         return (
             <div>
-                <form action="http://localhost:5000/api/fileupload" method="POST"encType="multipart/form-data" >
-                    <input id="choose-file" type="file" accept=".csv" name="upload"/>
-                    <label for="assessment-type">Assessment Type</label>
-                    <select name="assessmentType" id="assessment-type">
+                    <input id="choose-file" type="file" accept=".csv" name="upload" onChange={this.onFileChange}/>
+                    <label htmlFor="assessment-type">Assessment Type</label>
+                    <select value={this.state.assessmentType} name="assessmentType" id="assessmentType" onChange={this.setAssessmentType}>
                         <option value="selfAssessment">Self Assessment</option>
                         <option value="moduleChallenge">Module Challenge</option>
                     </select>
-                    <button id="upload" type="submit">Upload</button>
-                </form>
+                    <button id="upload" onClick={this.onFileUpload}>Upload</button>
+                    <br></br>
+                    {this.state.errors.map(error => <p style={{color: "red"}}>{error}</p> )}
             </div>
         );
     }
