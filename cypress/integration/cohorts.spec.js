960edb7291cdd1c1e5647f8d7c9da4837c59e7d2
Bridget <bridgetshidok@gmail.com>
2021-08-19 15:55:49 +0100

    Added Jest Tests

--- cypress/integration/cohorts.spec.js
+++ cypress/integration/cohorts.spec.js
@@ -2,8 +2,9 @@ describe("Cohorts", function() {
   beforeEach(function() {
     cy.intercept("http://localhost:5000/api/cohorts", { fixture: 'cohorts.json' })
   })
-  it("Shows a list of cohorts", function() {
-    cy.visit('/cohorts')
-    cy.get('#cohorts-list').should("contain", "MOCK cohort name")
+  it("Shows that the first data displayed are on the right format", function() {
+    cy.visit('/cohorts');
+    cy.get('#1').should("contain", "MOCK name");
+    cy.get('#1').should("contain", "MOCK date");
   })
 })
---
+++ src/components/Cohorts/mocks/cohort-data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  cohorts: [
+    { id: 1, name: 'MOCK name', startDate: 'MOCK date' },
+    { id: 2, name: 'MOCK name 2', startDate: 'MOCK date 2' },
+    { id: 3, name: 'MOCK name 3', startDate: 'MOCK date 3' }
+  ]
+}
