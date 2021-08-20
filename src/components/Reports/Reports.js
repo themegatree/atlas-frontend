aac37ec4391d50f695044cdcf98f4c449b3a20d0
Colin <cln.gardiner@outlook.com>
2021-08-20 12:13:43 +0100

    added doughnut charts
    Co-authored-by b7rch4 james.birchall4@btinternet.com

--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Doughnut } from "react-chartjs-2";
 import Report from './Report'
 import ReportHeader from "./ReportHeader";
+import buildData from "./buildData";
 
 class Reports extends Component {
     constructor(props) {
@@ -28,8 +30,10 @@ class Reports extends Component {
             <div>
                 <ReportHeader name={this.state.reports.cohortName} id={this.state.reports.cohortId} size={this.state.reports.cohortSize}/>
                 <Report name="gender" heading="Gender Distribution" data={this.state.reports.gender}/>
+                <Doughnut data={buildData(this.state.reports.gender)} />
                 <Report name="background" heading="Background Data" data={this.state.reports.background}/>
-                <Report name="challenge" heading="Student Challenge Completion" data={this.state.reports.challenges}/>
+                <Doughnut data={buildData(this.state.reports.background)} />
+                <Report name="challenge" heading="Student Challenge Completion" data={this.state.reports.challenges}/>         
             </div>
         )
     }
