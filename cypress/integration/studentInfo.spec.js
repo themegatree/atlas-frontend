4a3d017e542f731163bea409571e063721f875e5
Ryan Van Dijck <rvandijck02@outlook.com>
2021-08-23 10:50:06 +0100

    Updating cypress tests for new way of getting pic

--- cypress/integration/studentInfo.spec.js
+++ cypress/integration/studentInfo.spec.js
@@ -1,14 +1,15 @@
 describe("Student Info", function() {
     beforeEach(function() {
-        cy.intercept("http://localhost:5000/api/student:1", { fixture: 'studentInfo.json' })
-        cy.intercept("https://github.com/testname.png/", { fixture: 'exampleProfile.png' })
+        cy.intercept("http://localhost:5000/api/students:1", { fixture: 'studentInfo.json' })
+        cy.intercept("https://api.github.com/users/testname", { fixture: 'studentInfoImageLink.json' })
+        cy.intercept("http://localhost:5000/api/mock", {fixture: 'exampleProfile.png'})
     })
     it("Student Info data test", function() {
         cy.visit('/student/1')
         cy.get('#id').should('contain', '1') 
-        cy.get('#firstName').should('contain', 'Test') 
+        cy.get('#firstName').should('contain', 'Mike') 
         cy.get('#lastName').should('contain', 'Testerson') 
-        cy.get('#github').should('contain', 'zcbenz') 
+        cy.get('#github').should('contain', 'testname') 
         cy.get('#email').should('contain', 'real@email.com') 
         cy.get('#githubImage').should('exist')
     })
