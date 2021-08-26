21632974b492c346b5cc4f7ae3a5dcb2fff3581d
Nkosilathi Sibanda <kosi53@hotmail.co.uk>
2021-08-26 12:52:31 +0100

    Display challenges and completion statuses of a student

--- src/components/Students/StudentInfo.js
+++ src/components/Students/StudentInfo.js
@@ -6,38 +6,60 @@ const GetStudentInfo = (props) => {
 }
 
 class StudentInfo extends Component{
-
     constructor(props) {
         super(props)
         this.state = {
-            studentData: {} 
+            studentData: {},
+            studentChallenges: {}
         }
       }
 
     async componentDidMount(){
         await fetch(`${process.env.REACT_APP_API_URL}/api/students/${this.props.id}`)
         .then(res => res.json())
-        .then(data => this.setState({studentData: data.student}))
+        .then(data => this.setState({studentData: data.student, studentChallenges: data.studentChallenges}))
         await fetch(`https://api.github.com/users/${this.state.studentData.githubUsername}`)
         .then(res => res.json())
-        .then(data =>  this.setState({avatar_Url: data.avatar_url}))
+        .then(data =>  this.setState({avatarUrl: data.avatar_url}))
     }
+
     render(){
         return (
             <div>
-                <img alt="Github Profile Not Found" id="githubImage" src={this.state.avatar_Url}></img>
-                <br></br>
-                <p id="id">ID: {this.props.id}</p>
-                <br></br>
-                <p id="firstName">First Name: {this.state.studentData.firstName}</p>
-                <br></br>
-                <p id="lastName">Last Name: {this.state.studentData.lastName}</p>
-                <br></br>
-                <p id="github">Github Username: {this.state.studentData.githubUsername}</p>
-                <br></br>
-                <p id="email">Email: {this.state.studentData.email}</p>
+                <img alt="Github Profile Not Found" id="githubImage" src={this.state.avatarUrl}></img>
+                <br />
+                <p id="id"><b>ID</b>: {this.props.id}</p>
+                <p id="firstName"><b>First Name:</b> {this.state.studentData.firstName}</p>
+                <p id="lastName"><b>Last Name:</b> {this.state.studentData.lastName}</p>
+                <p id="github"><b>Github Username:</b> {this.state.studentData.githubUsername}</p>
+                <p id="email"><b>Email:</b> {this.state.studentData.email}</p>
+                <h3>Challenges</h3>
+                {<table>
+                    <tr>
+                        <th>Challenge</th>
+                        <th>Student Score</th>
+                        <th>Coach Score</th>
+                        <th>Submission Date</th>
+                        <th>Completion Status</th>
+                    </tr>
+                        {this.state.studentChallenges.length > 0 &&
+                            this.state.studentChallenges.map((challenge, index) => (
+                                <tr>
+                                    <td>{challenge.challengeName}</td>
+                                    <td>{challenge.studentScore}</td>
+                                    <td>{challenge.coachScore}</td>
+                                    {challenge.submissionDate.length > 0 ? 
+                                        <td>{challenge.submissionDate.slice(0, 10)}</td> : <td>No submission date</td>
+                                    }
+                                    {challenge.submissionDate.length > 0 ? 
+                                        <td>Submitted</td> : <td>Not submitted</td>
+                                    }
+                                </tr>
+                            ))
+                        }
+                    </table>}
+                    <br />
             </div>
-
         )
     }
 }
