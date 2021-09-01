4d86391e2c29c54b8aff9eb3697986bf147e5810
Rawdah <rawdah98@outlook.com>
2021-09-01 18:05:23 +0100

    Finished Bootstrap style cards

--- src/components/Cohorts/Cohort.js
+++ src/components/Cohorts/Cohort.js
@@ -1,27 +1,19 @@
 import React from "react";
-import {useRouteMatch, Link, BrowserRouter as Router,} from "react-router-dom";
+import {useRouteMatch} from "react-router-dom";
 
 const Cohort = (props) => {
 
   const date = () =>  props.cohort.startDate.substring(0,10)
-  
-
-  let {  url } = useRouteMatch();
+    
+  let { url } = useRouteMatch();
 
   return (
-       <a href = {`${url}/${props.cohort.id}`} className="card">
-       <div className="col-md" id= {props.cohort.id} >
-      <p data-testid="name">Name: {props.cohort.name}</p>
-      <p  data-testid="date" >Start Date: {date()}</p>
-   
-
-      {/* <Router>
-     <li className="link" data-testid="link">
-       <Link data-testid="link" id="link" to={`${url}/${props.cohort.id}`}>View details »</Link>
-      </li>
-      </Router> */}
-      </div>
-       </a>
+      <a href = {`${url}/${props.cohort.id}`} className="card">
+        <div className="col-md" id= {props.cohort.id} >
+          <p data-testid="name">Name: {props.cohort.name}</p>
+          <p data-testid="date" >Start Date: {date()}</p>
+        </div>
+      </a>
           
   )
 }
