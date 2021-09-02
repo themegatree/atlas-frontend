d2aec3bcb4926e70edd5c4a1a7cf5749c2e1c35c
unknown <benclayton1@hotmail.com>
2021-09-02 15:57:02 +0100

    ensured that bullet points removed from list

--- src/components/Upload/Entry.js
+++ src/components/Upload/Entry.js
@@ -25,8 +25,8 @@ changeCond(){
 
   render(){
     return(
-      <p id={`Text-${this.state.id}`} className="historyHeader" style={{color: this.state.data.status === "Success" ? 'green' : 'red'}}>{this.state.data.status} {this.state.data.uploadType} {this.state.date} <button id={`Button-${this.state.id}`} className="btn btn-history" onClick={() => this.changeCond()}>Check</button><ul id={`Text-${this.state.id}`} className="errors" style={{fontSize: this.state.check ? 14 : 0}}>{this.state.errors.map(error => <li>{error}</li>)}</ul></p>
-    )
+      <p id={`Text-${this.state.id}`} className="historyHeader" style={{color: this.state.data.status === "Success" ? 'green' : 'red'}}>{this.state.data.status} {this.state.data.uploadType} {this.state.date} <button id={`Button-${this.state.id}`} className="btn btn-history" onClick={() => this.changeCond()}>Check</button><ul id={`Text-${this.state.id}`} className="errors" style={{fontSize: this.state.check ? 14 : 0}}>{this.state.errors.map(error => <li className="list">{error}</li>)}</ul></p>
+      )
   }
 
 }
