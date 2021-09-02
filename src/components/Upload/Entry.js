841487c300dcefcbf8e190d90bb8707e663d6028
Neokrieger <theneokrieger@hotmail.com>
2021-09-02 12:01:32 +0100

    Removed bullet points on list items

--- src/components/Upload/Entry.js
+++ src/components/Upload/Entry.js
@@ -23,7 +23,7 @@ changeCond(){
 
   render(){
     return(
-      <p id={`Text-${this.state.id}`}  style={{color: this.state.data.status === "Success" ? 'green' : 'red'}}>{this.state.data.status} {this.state.data.uploadType} {this.state.date} <button id={`Button-${this.state.id}`} onClick={() => this.changeCond()}>Check</button><ul id={`Text-${this.state.id}`} style={{fontSize: this.state.check ? 12 : 0}}>{this.state.errors.map(error => <li>{error}</li>)}</ul></p>
+      <p id={`Text-${this.state.id}`}  style={{color: this.state.data.status === "Success" ? 'green' : 'red'}}>{this.state.data.status} {this.state.data.uploadType} {this.state.date} <button id={`Button-${this.state.id}`} onClick={() => this.changeCond()}>Check</button><ul id={`Text-${this.state.id}`} style={{fontSize: this.state.check ? 12 : 0}}>{this.state.errors.map(error => <li style={{listStyle: "none"}}>{error}</li>)}</ul></p>
     )
   }
 
