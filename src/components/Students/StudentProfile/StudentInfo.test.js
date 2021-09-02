bf65ed8632179032d3921ea977f8d143d70f4a8d
Nkosilathi Sibanda <kosi53@hotmail.co.uk>
2021-09-02 14:04:16 +0100

    Added more test data and updated the test for displaying student info

--- src/components/Students/StudentProfile/StudentInfo.test.js
+++ src/components/Students/StudentProfile/StudentInfo.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {StudentInfo} from './StudentInfo.js'
 import data from './__mocks__/student-info-data.js'
 
@@ -10,16 +9,39 @@ beforeEach(() => {
   test('Displays Student Info', async () => {
     render(
       <StudentInfo />
-    );
-    const lastNameElement = await screen.findByText(/Testerson/i);
-    expect(lastNameElement).toBeInTheDocument();
+    )
 
-    const emailElement = await screen.findByText(/real@email.com/i);
-    expect(emailElement).toBeInTheDocument();
+    const imageElement = screen.getByRole('img')
+    expect(imageElement).toHaveAttribute('alt', 'Github Profile Not Found')
+    expect(imageElement).toBeInTheDocument()
 
     const firstNameElement = await screen.findByText(/Mike/i);
-    expect(firstNameElement).toBeInTheDocument();
+    expect(firstNameElement).toBeInTheDocument()
+
+    const lastNameElement = await screen.findByText(/Testerson/i);
+    expect(lastNameElement).toBeInTheDocument()
 
     const githubElement = await screen.findByText(/testname/i);
-    expect(githubElement).toBeInTheDocument();
+    expect(githubElement).toBeInTheDocument()
+
+    const emailElement = await screen.findByText(/real@email.com/i);
+    expect(emailElement).toBeInTheDocument()
+    
+    const challengeName = await screen.findByText(/Bank/i);
+    expect(challengeName).toBeInTheDocument()
+
+    const language = await screen.findByText(/NodeJS/i);
+    expect(language).toBeInTheDocument()
+
+    const studentScore = await screen.findByText(/incomplete/i, {exact: true})
+    expect(studentScore).toBeInTheDocument()
+
+    const coachScore = await screen.findByText(/incomplete/i, {exact: true});
+    expect(coachScore).toBeInTheDocument()
+
+    const dueDate = await screen.findByText(/2021-08-07/i);
+    expect(dueDate).toBeInTheDocument()
+
+    const submissionDate = await screen.findByText(/2021-08-06/i);
+    expect(submissionDate).toBeInTheDocument()
   });
--- src/components/Students/StudentProfile/__mocks__/student-info-data.js
+++ src/components/Students/StudentProfile/__mocks__/student-info-data.js
@@ -1,8 +1,21 @@
-module.exports ={"student":{
-    "id": "1",
+module.exports = {"student":{
+    "id": 1,
     "firstName": "Mike",
     "lastName": "Testerson",
     "githubUsername": "testname",
-    "email": "real@email.com"},
-    "avatar_url": "./__mocks__/exampleProfile.png"
+    "email": "real@email.com",
+    "avatar_url": "./__mocks__/exampleProfile.png",
+    "ModuleChallenges": [
+        {"id":1,
+            "StudentId":1,
+            "challengeName":"Bank",
+            "language":"NodeJS",
+            "studentScore":"complete",
+            "coachScore":"incomplete",
+            "dueDate":"2021-08-07T00:00:00.000Z",
+            "submissionDate": "2021-08-06T00:00:00.000Z",
+            "createdAt":"2021-08-29T00:00:00.000Z",
+            "updatedAt":"2021-08-29T00:00:00.000Z"
+        }],
+    }
 }
