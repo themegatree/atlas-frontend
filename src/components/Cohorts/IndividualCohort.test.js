7b21dd34280747dcd8d5efcc61804aa365191f9d
Bridget <bridgetshidok@gmail.com>
2021-09-03 15:02:32 +0100

    added more tests

--- src/components/Cohorts/IndividualCohort.test.js
+++ src/components/Cohorts/IndividualCohort.test.js
@@ -21,15 +21,23 @@ test('Check Cohorts details', async () => {
   const headerElement = await screen.findByText(/Cohort's Information/i)
   expect(headerElement).toBeInTheDocument();
 
-  const nameElement = await screen.findByText("Cohort Name: Cohort 1")
+  const nameElement = await screen.findByText("Cohort Name: mse-2109-a")
   expect(nameElement).toBeInTheDocument();
 
-  const dateElement = await screen.findByText("Start Date: 2021-02-28")
+  const dateElement = await screen.findByText("Start Date: 2021-06-27T23:00:00.000Z")
   expect(dateElement).toBeInTheDocument();
 
-  const leadCoachElement = await screen.findByText("Start Date: 2021-02-28")
+  const leadCoachElement = await screen.findByText("Lead Coach: John")
   expect(leadCoachElement).toBeInTheDocument();
 
+  const studentFirstNameElement = await screen.findByTestId("firstName")
+  expect(studentFirstNameElement).toHaveTextContent("First Name:Clive")
+
+  const studentLastNameElement = await screen.findByTestId("lastName")
+  expect(studentLastNameElement).toHaveTextContent("Last Name:Smith")
+
+  const studentLinkElement = await screen.findByTestId("report")
+  expect(studentLinkElement).toHaveTextContent("Go To Report")
 });
 
 
--- src/components/Cohorts/__mocks__/individual-cohort-data.js
+++ src/components/Cohorts/__mocks__/individual-cohort-data.js
@@ -1,41 +1,25 @@
 module.exports = {
-  cohort: {
-    "id": "1",
-      "name": "Cohort 1",
-      "startDate": "2021-02-28",
-      "cohortSize": "2",
-      "leadCoach": "Ed",
-      "Students": [
-        {
-            "id": 1,
-            "firstName": "Fred",
-            "lastName": "Marshall",
-            "githubUsername": "zaramgit",
-            "email": "zaram@email.com",
-            "background": "white",
-            "gender": "male",
-            "createdAt": "2021-09-02T17:28:21.363Z",
-            "updatedAt": "2021-09-02T17:28:21.378Z",
-            "CohortId": 1,
-            "ModuleChallenges": [],
-            "SelfAssessments": [],
-            "Cohort": null
-        },
-        {
-            "id": 2,
-            "firstName": "Betty",
-            "lastName": "Clark",
-            "githubUsername": "betty_real",
-            "email": "betty-clark@email.com",
-            "background": "Black",
-            "gender": "Female",
-            "createdAt": "2021-09-02T17:28:21.363Z",
-            "updatedAt": "2021-09-02T17:28:21.378Z",
-            "CohortId": 1,
-            "ModuleChallenges": [],
-            "SelfAssessments": [],
-            "Cohort": null
-        }
-      ]
+  "cohort": {
+    "cohortSize": 1,
+    "id": 114,
+    "name": "mse-2109-a",
+    "startDate": "2021-06-27T23:00:00.000Z",
+    "leadCoach": "John",
+    "createdAt": "2021-09-03T13:22:11.592Z",
+    "updatedAt": "2021-09-03T13:22:11.592Z",
+    "Students": [
+      {
+        "id": 4,
+        "firstName": "Clive",
+        "lastName": "Smith",
+        "githubUsername": "csmithgit",
+        "email": "clivesmith@email.com",
+        "background": "Black",
+        "gender": "Female",
+        "createdAt": "2021-09-02T17:28:21.363Z",
+        "updatedAt": "2021-09-02T17:28:21.363Z",
+        "CohortId": 114
+      }
+    ]
   }
 }
