1fba48ac26a8ca45f1a09e37521bb2e39a2ef8ce
Shivam Sheth <shivam@shethmail.co.uk>
2021-09-03 11:42:36 +0100

    Remove comment

--- src/components/Students/StudentList/Student.js
+++ src/components/Students/StudentList/Student.js
@@ -13,7 +13,6 @@ const Student = (props) => {
       <p>Github Username: {props.student.githubUsername}</p>
       <p>Email: {props.student.email}</p>
       </a>
-      {/* <li className="btn btn-secondary"><a href={`${url}/${props.student.id}`} id={`button-${props.index}`}>View Profile</a></li> */}
     </div>
   )
 }
