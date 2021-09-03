a7d751cd588c2d3a8011d1871e82793582c0da32
Ryan Van Dijck <71043499+RyanVanDijck@users.noreply.github.com>
2021-09-03 10:58:27 +0100

    At 60 implement editing student info on the frontend (#25)
    * Updating Student Info on frontend
    
    * Storing student data after update
    
    * Creating cypress tests
    
    * Making the profile picture appear again.
    
    * Adding messages when the fetch fails
    
    * Finalising react tests
    
    * Removing unused function arguments
    
    * Replacing fireEvent with userEvent
    
    * Cleaning up render function
    
    * Cleaning up setStates
    
    * Changing the way that the data is passed to update
    
    * Changing the way that the data is passed to update
    
    * Fixing react mock for failing to retrieve profile
    
    * Getting status of api request from api response
    
    * Fixing issues related to rebase
    
    * Trying to fix css related bug
    
    * Adding catches to fetches to statisfy eslint
    
    * Formatting react tests
    
    * Fixing cypress tests
    
    * Getting status of api request from api response
    
    * Added Jest Tests
    
    * fixed conflict
    
    * sorting jenkins
    
    * passed cypress test
    
    * Update directories
    
    * Fixing issues related to rebase
    
    * Trying to fix css related bug
    
    * Adding catches to fetches to statisfy eslint
    
    * Formatting react tests
    
    * Fixing cypress tests
    
    * Incoporating error messages from api
    
    * Refactors
    
    - Specify URLS to intercept by HTTP method, using RouteMatcher
    - Updated filenames
    - Reorganised Cypress test for clarity
    - Updated the studentInfo component lifecycle method (CDM)
    
    * Adding test for when name is too long
    
    * Removing comments
    
    * Changing the error message to match the api
    
    * Fixing Broken Test
    
    Co-authored-by: Shivam Sheth <shivam@shethmail.co.uk>
    Co-authored-by: dearshrewdwit <dearshrewdwit@gmail.com>

--- src/components/Students/StudentProfile/StudentInfo.js
+++ src/components/Students/StudentProfile/StudentInfo.js
@@ -1,7 +1,7 @@
 import React, { Component} from "react"
 import { useParams } from "react-router-dom";
 
-const GetStudentInfo = (props) => {
+const GetStudentInfo = () => {
     return (<StudentInfo id={useParams().id}/>)
 }
 
@@ -10,35 +10,97 @@ class StudentInfo extends Component{
     constructor(props) {
         super(props)
         this.state = {
-            studentData: {} 
+            studentData: {},
+            showTextbox: false,
+            response: ""
         }
       }
 
-    async componentDidMount(){
-        await fetch(`${process.env.REACT_APP_API_URL}/api/students/${this.props.id}`)
+    componentDidMount(){
+      fetch(`${process.env.REACT_APP_API_URL}/api/students/${this.props.id}`)
         .then(res => res.json())
-        .then(data => this.setState({studentData: data.student}))
-        await fetch(`https://api.github.com/users/${this.state.studentData.githubUsername}`)
-        .then(res => res.json())
-        .then(data =>  this.setState({avatar_Url: data.avatar_url}))
+        .then(apiData => {
+          fetch(`https://api.github.com/users/${apiData.student.githubUsername}`)
+            .then(res => res.json())
+            .then(githubData => this.setState({
+              studentData: apiData.student,
+              avatar_Url: githubData.avatar_url
+            }))
+        })
     }
-    render(){
+
+    mainPage = () => {
         return (
-            <div>
+            <section>
+                <p id='response'>{this.state.response}</p>
                 <img alt="Github Profile Not Found" id="githubImage" src={this.state.avatar_Url}></img>
                 <br></br>
                 <p id="id">ID: {this.props.id}</p>
                 <br></br>
                 <p id="firstName">First Name: {this.state.studentData.firstName}</p>
+                <button id="firstNameUpdate" onClick={() => {this.showTextbox("First Name", "firstName")}}>Change First Name</button>
                 <br></br>
                 <p id="lastName">Last Name: {this.state.studentData.lastName}</p>
+                <button id="lastNameUpdate" onClick={() => {this.showTextbox("Last Name", "lastName")}}>Change Last Name</button>
                 <br></br>
                 <p id="github">Github Username: {this.state.studentData.githubUsername}</p>
+                <button id="githubUsernameUpdate" onClick={() => {this.showTextbox("Github Username", "githubUsername")}}>Change GitHub Username</button>
                 <br></br>
                 <p id="email">Email: {this.state.studentData.email}</p>
-            </div>
+                <button id="emailUpdate" onClick={() => {this.showTextbox("Email", "email")}}>Change Email</button>
+            </section>
 
         )
+    }
+
+
+    submitField = async (e) => {
+        e.preventDefault()
+        this.setState({showTextbox: false})
+        const data = {}
+        data[this.state.columnName] = e.target.childNodes[1].value
+        const opts = {
+            method: 'PUT',
+            body: data
+        }
+        await fetch(`${process.env.REACT_APP_API_URL}/api/students/${this.props.id}`, opts
+        ).then(res => res.json())
+        .then(data => {
+            if (data.status === 'success'){
+                this.setState({studentData: data.student, response: 'Data Updated Successfully'})
+            }
+            else{
+                this.setState({response: data.errors})
+            }
+        })
+        .catch()
+    }
+
+    textbox = () => {
+        const currentValue = this.state.studentData[this.state.columnName]
+        return(
+            <section>
+                <form onSubmit={this.submitField}>
+                    <p>{this.state.displayName}: </p>
+                    <input id="updateInput" placeholder={currentValue}></input>
+                    <input type="submit" value="Update" id="update"></input>
+                </form>
+            </section>
+        )
+    }
+
+    showTextbox = (displayName, columnName) => {
+        this.setState({
+            showTextbox: true,
+            displayName: displayName,
+            columnName: columnName
+        });
+    }
+
+
+
+    render(){
+        return (this.state.showTextbox) ? this.textbox() : this.mainPage()
     }
 }
 
--- src/components/Students/StudentProfile/StudentInfo.test.js
+++ src/components/Students/StudentProfile/StudentInfo.test.js
@@ -1,25 +1,96 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import {StudentInfo} from './StudentInfo.js'
 import data from './__mocks__/student-info-data.js'
+import updatedData from './__mocks__/student-info-data-update.js'
+import failData from './__mocks__/student-info-fail.js'
+
 
 beforeEach(() => {
-    global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(data)}))
-  })
+  global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(data)}))
+}); 
   
-  test('Displays Student Info', async () => {
-    render(
-      <StudentInfo />
-    );
-    const lastNameElement = await screen.findByText(/Testerson/i);
-    expect(lastNameElement).toBeInTheDocument();
+test('Displays Student Info', async () => {
+  render(
+    <StudentInfo />
+  )
 
-    const emailElement = await screen.findByText(/real@email.com/i);
-    expect(emailElement).toBeInTheDocument();
+  const lastNameElement = await screen.findByText(/Testerson/i);
+  expect(lastNameElement).toBeInTheDocument();
 
-    const firstNameElement = await screen.findByText(/Mike/i);
-    expect(firstNameElement).toBeInTheDocument();
+  const emailElement = await screen.findByText(/real@email.com/i);
+  expect(emailElement).toBeInTheDocument();
 
-    const githubElement = await screen.findByText(/testname/i);
-    expect(githubElement).toBeInTheDocument();
-  });
+  const firstNameElement = await screen.findByText(/Mike/i);
+  expect(firstNameElement).toBeInTheDocument();
+
+  const githubElement = await screen.findByText(/testname/i);
+  expect(githubElement).toBeInTheDocument();
+});
+
+test('Update Student Info' , async () => {
+  render(
+    <StudentInfo />
+  )
+  
+  const firstNameSubmitButton = await screen.findByText(/Change First Name/i)
+  userEvent.click(firstNameSubmitButton)
+
+  const firstNameInput = await screen.findByPlaceholderText(/Mike/i)
+  userEvent.type(firstNameInput,'Jack')
+
+  global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(updatedData)}))
+
+  const submitButton = await screen.findByText(/Update/i)
+  userEvent.click(submitButton)
+
+  const lastNameElement = await screen.findByText(/Testerson/i);
+  expect(lastNameElement).toBeInTheDocument();
+
+  const emailElement = await screen.findByText(/real@email.com/i);
+  expect(emailElement).toBeInTheDocument();
+
+  const firstNameElement = await screen.findByText(/Jack/i);
+  expect(firstNameElement).toBeInTheDocument();
+
+  const githubElement = await screen.findByText(/testname/i);
+  expect(githubElement).toBeInTheDocument();
+
+  const checkElement = await screen.findByText(/Data Updated Successfully/i);
+  expect(checkElement).toBeInTheDocument();
+});
+
+test('Fail to update student info', async () => {
+  render(
+    <StudentInfo />
+  )
+
+  const firstNameSubmitButton = await screen.findByText(/Change First Name/i)
+  userEvent.click(firstNameSubmitButton)
+  
+  global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(failData)}))
+
+  const firstNameInput = await screen.findByPlaceholderText(/Mike/i)
+  userEvent.type(firstNameInput,'Jack')
+
+  const submitButton = await screen.findByText(/Update/i)
+  userEvent.click(submitButton)
+
+  global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(updatedData)}))
+
+  const lastNameElement = await screen.findByText(/Testerson/i);
+  expect(lastNameElement).toBeInTheDocument();
+
+  const emailElement = await screen.findByText(/real@email.com/i);
+  expect(emailElement).toBeInTheDocument();
+
+  const firstNameElement = await screen.findByText(/Mike/i);
+  expect(firstNameElement).toBeInTheDocument();
+
+  const githubElement = await screen.findByText(/testname/i);
+  expect(githubElement).toBeInTheDocument();
+
+  const checkElement = await screen.findByText(/Invalid params/i);
+  expect(checkElement).toBeInTheDocument();
+
+})
